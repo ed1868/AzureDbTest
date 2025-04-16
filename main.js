61f2db8c344ba9c1584f5ab890bd35adf2aa6ee0
Eddie Ruiz <ruizeduardo21@gmail.com>
2025-04-16 01:54:09 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-console.log("hello world")
+const sql = require('mssql');
+
+const config = {
+  server: process.env.DB_SERVER,
+  database: process.env.DB_DATABASE,
+  authentication: {
+    type: 'azure-active-directory-password',
+    options: {
+      userName: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD
+    }
+  },
+  options: {
+    encrypt: true,
+    enableArithAbort: true,
+  }
+};
+
+async function connectToAzureDB() {
+  try {
+    let pool = await sql.connect(config);
+    console.log("Connected successfully to Azure DB");
+    let result = await pool.request().query("SELECT DB_NAME() AS [CurrentDatabase]");
+    console.log(result.recordset);
+    await sql.close();
+  } catch (err) {
+    console.error("Failed to connect to Azure DB", err);
+  }
+}
+
+connectToAzureDB();
